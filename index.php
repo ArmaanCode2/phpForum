6b45b3a18583b8a1ce0167724f6b7106e86f307c
ArmaanCode2 <armaansecond9@gmail.com>
2022-03-22 21:05:53 +0530

    Forum vid 48 completed

--- index.php
+++ index.php
@@ -14,11 +14,31 @@
 
 <body>
     <?php include 'partials/_header.php';?>
+
+      <div class="container my-3">
+        <h2 class="text-center my-3">iDiscuss - Browse Categories</h2>
+        <div class="row">
+
+  <!-- use a for loop to iterate through Categories -->
+          <div class="col-md-4 my-3">
+            <div class="card" style="width: 18rem;">
+                <img src="https://source.unsplash.com/500x400/?coding,python" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">View Threads</a>
+                </div>
+              </div>
+          </div>
+
+        </div>
+      </div>
+
     <?php include 'partials/_footer.php';?>
 
 
 
-    
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
     </script>
